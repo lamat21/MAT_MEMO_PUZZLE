fb072688f82d06f4faf04d9ddd9f29eab7e161e0
Sutharsini Ravichandra <suthyr@my.yorku.ca>
2022-04-02 18:53:58 -0400

    Essy level code try 2

--- app/src/main/java/com/example/mat_memo_puzzle/EasyLevel.java
+++ app/src/main/java/com/example/mat_memo_puzzle/EasyLevel.java
@@ -1,7 +1,6 @@
 package com.example.mat_memo_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,17 +16,18 @@ public class EasyLevel extends AppCompatActivity {
     private TextView DisplayList;
     private TextView DisplayInstructions;
     private EditText userInput;
+    GameBoard game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_level);
 
-        DisplayList = (TextView) findViewById(R.id.DisplayList);
-        DisplayInstructions = (TextView) findViewById(R.id.DisplayInstructions);
+        DisplayList = findViewById(R.id.DisplayList);
+        DisplayInstructions = findViewById(R.id.DisplayInstructions);
 
         boolean doWeKeepPlaying = true;
-        GameBoard game = new GameBoard(1);
+        game = new GameBoard(1);
         try {
             while (doWeKeepPlaying){
                 //Wait so user can read the starting massage
@@ -52,22 +52,19 @@ public class EasyLevel extends AppCompatActivity {
 
                 //Get users answer
                 Thread.sleep(1000);
+                submitBtn = (Button) findViewById(R.id.submitBtn);
                 ArrayList<Integer> userAnswer = new ArrayList<Integer>();
-
-                for (int i = 0; i < game.getNumberList().size(); i++) {
-                    DisplayInstructions.setText("Enter Number " + (i + 1));
-                    int num = Integer.parseInt(userInput.getText().toString());
-                    userAnswer.add(num);
-                }
+                userAnswer = enterInput(userAnswer);
 
                 //Check answer and give feedback
+                openActivitySubmitBtn(userAnswer);
                 if (game.checkAnswer(userAnswer)){
                     game.resetGameBoard(1);
                 } else {
                     doWeKeepPlaying = false;
                 }
             }
-            openActivitySubmitBtn();
+            //openActivitySubmitBtn();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -80,21 +77,34 @@ public class EasyLevel extends AppCompatActivity {
                 openActivityPauseBtn();
             }
         });
-        submitBtn = (Button) findViewById(R.id.submitBtn);
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivitySubmitBtn();
+    }
+
+        private ArrayList<Integer> enterInput(ArrayList<Integer> userAnswer) {
+            for (int i = 0; i < game.getNumberList().size(); i++) {
+                DisplayInstructions.setText("Enter Number " + (i + 1));
+                submitBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int num = Integer.parseInt(userInput.getText().toString());
+                        userAnswer.add(num);
+                    }
+                });
             }
+            return userAnswer;
+        }
 
-        });
-    }
-    public void openActivityPauseBtn() {
-        Intent pausebtn = new Intent(this, MenuBtn.class);
-        startActivity(pausebtn);
-    }
-    public void openActivitySubmitBtn() {
 
-        Intent submitBtn = new Intent(this, SubmitBtn.class);
-        startActivity(submitBtn);}
+        public void openActivityPauseBtn() {
+            Intent pausebtn = new Intent(this, MenuBtn.class);
+            startActivity(pausebtn);
+        }
+        public void openActivitySubmitBtn(ArrayList<Integer> answer) {
+            if (game.checkAnswer(answer)){
+                game.resetGameBoard(1);
+            } else {
+                Intent submitBtn = new Intent(this, SubmitBtn.class);
+                startActivity(submitBtn);
+            }
+        }
+
     }
