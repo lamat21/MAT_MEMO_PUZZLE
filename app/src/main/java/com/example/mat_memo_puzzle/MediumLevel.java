9014b93b827d8fcf7d3cb24c974526f405c537a2
lamatamer <lamat21@my.yorku.ca>
2022-04-01 21:02:15 -0400

    easy,medum,hard works

--- app/src/main/java/com/example/mat_memo_puzzle/MediumLevel.java
+++ app/src/main/java/com/example/mat_memo_puzzle/MediumLevel.java
@@ -2,13 +2,45 @@ package com.example.mat_memo_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MediumLevel extends AppCompatActivity {
-
+    private Button pauseBtn;
+    private Button submitBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medium_level);
+
+        pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityPauseBtn();
+            }
+        });
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitySubmitBtn();
+            }
+
+        });
+    }
+    public void openActivityPauseBtn() {
+        Intent pausebtn = new Intent(this, MenuBtn.class);
+        startActivity(pausebtn);
+    }
+    public void openActivitySubmitBtn() {
+
+        if (!false){
+            // game.checkanswer if false show end screen/popup, else reset gameboard and continue.
+
+            Intent submitBtn = new Intent(this, SubmitBtn.class);
+            startActivity(submitBtn);}
     }
 }
--- app/src/main/java/com/example/mat_memo_puzzle/MenuBtn.java
+++ app/src/main/java/com/example/mat_memo_puzzle/MenuBtn.java
@@ -2,13 +2,57 @@ package com.example.mat_memo_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuBtn extends AppCompatActivity {
-
+    private Button settingsButton;
+    private Button instructionButton;
+    private Button quitGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_btn);
+
+        settingsButton = (Button) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityPopSettings();
+            }
+
+        });
+        instructionButton = (Button) findViewById(R.id.instructionButton);
+        instructionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openActivityPopIntrctions();
+            }
+
+        });
+        quitGame = (Button) findViewById(R.id.quitGame);
+        quitGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitymainActivity();
+            }
+
+        });
+
+    }
+    public void openActivityPopSettings() {
+        Intent popSettings = new Intent(this, PopSettings.class);
+        startActivity(popSettings);
+    }
+    public void openActivityPopIntrctions() {
+        Intent popInstructions = new Intent(this, popInstructions.class);
+        startActivity(popInstructions);
+    }
+    public void openActivitymainActivity() {
+        Intent homeButton = new Intent(this, MainActivity.class);
+        startActivity(homeButton);
     }
 }
