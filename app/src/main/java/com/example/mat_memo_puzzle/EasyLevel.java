b8298fed202c19f5d648cd98066a2fa8cc202f3a
Sutharsini Ravichandra <suthyr@my.yorku.ca>
2022-04-01 22:46:02 -0400

    easy java implementation try one

--- app/src/main/java/com/example/mat_memo_puzzle/EasyLevel.java
+++ app/src/main/java/com/example/mat_memo_puzzle/EasyLevel.java
@@ -6,14 +6,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class EasyLevel extends AppCompatActivity {
     private Button pauseBtn;
     private Button submitBtn;
+    private TextView DisplayList;
+    private TextView DisplayInstructions;
+    private EditText userInput;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_level);
+
+        DisplayList = (TextView) findViewById(R.id.DisplayList);
+        DisplayInstructions = (TextView) findViewById(R.id.DisplayInstructions);
+
+        boolean doWeKeepPlaying = true;
+        GameBoard game = new GameBoard(1);
+        try {
+            while (doWeKeepPlaying){
+                //Wait so user can read the starting massage
+                Thread.sleep(1000);
+
+                for (Integer i: game.getNumberList()) {
+                    DisplayList.setText(i);
+                    Thread.sleep(1250);
+                    DisplayList.setText("");
+                    Thread.sleep(1250);
+                }
+
+                //Displays the list of numbers in a scrambled order
+                DisplayInstructions.setText("Let's test your memory!");
+                ArrayList<Integer> scrambledList = game.scramble();
+                String scrambledListOfNum = "";
+                for (Integer i: scrambledList) {
+                    scrambledListOfNum += i;
+                    scrambledListOfNum += "   ";
+                }
+                DisplayList.setText(scrambledListOfNum);
+
+                //Get users answer
+                Thread.sleep(1000);
+                ArrayList<Integer> userAnswer = new ArrayList<Integer>();
+
+                for (int i = 0; i < game.getNumberList().size(); i++) {
+                    DisplayInstructions.setText("Enter Number " + (i + 1));
+                    int num = Integer.parseInt(userInput.getText().toString());
+                    userAnswer.add(num);
+                }
+
+                //Check answer and give feedback
+                if (game.checkAnswer(userAnswer)){
+                    game.resetGameBoard(1);
+                } else {
+                    doWeKeepPlaying = false;
+                }
+            }
+            openActivitySubmitBtn();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         pauseBtn = (Button) findViewById(R.id.pauseBtn);
         pauseBtn.setOnClickListener(new View.OnClickListener() {
@@ -37,10 +95,6 @@ public class EasyLevel extends AppCompatActivity {
     }
     public void openActivitySubmitBtn() {
 
-        if (!false){
-          // game.checkanswer if false show end screen/popup, else reset gameboard and continue.
-
         Intent submitBtn = new Intent(this, SubmitBtn.class);
         startActivity(submitBtn);}
     }
-}
