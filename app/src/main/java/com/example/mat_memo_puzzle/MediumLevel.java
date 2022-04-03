c179806c47b70a5be0fbb40a78f9236d08405978
Sutharsini Ravichandra <suthyr@my.yorku.ca>
2022-04-02 21:38:16 -0400

    mediumLevel java with states

--- app/src/main/java/com/example/mat_memo_puzzle/MediumLevel.java
+++ app/src/main/java/com/example/mat_memo_puzzle/MediumLevel.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MediumLevel extends AppCompatActivity {
     private Button pauseBtn = findViewById(R.id.pauseBtn);
     private Button submitBtn = findViewById(R.id.submitBtn);
@@ -16,17 +18,15 @@ public class MediumLevel extends AppCompatActivity {
     private TextView DisplayInstructions = findViewById(R.id.displayInstructions);
     private EditText userInput = findViewById(R.id.userInput);
     GameBoard game;
+    private Stats userStats = new Stats();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medium_level);
 
-        game = new GameBoard(1);
-
-        displayRandNumList();
-
-        displayScrambledNumList();
+        //Set Stats
+        userStats = getIntent().getParcelableExtra("userStats");
 
         pauseBtn = (Button) findViewById(R.id.pauseBtn);
         pauseBtn.setOnClickListener(new View.OnClickListener() {
@@ -35,27 +35,34 @@ public class MediumLevel extends AppCompatActivity {
                 openActivityPauseBtn();
             }
         });
-        submitBtn = (Button) findViewById(R.id.submitBtn);
-        submitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivitySubmitBtn();
-            }
 
-        });
+        game = new GameBoard(1);
+        boolean keepPlaying = true;
+
+        while (keepPlaying){
+            displayRandNumList();
+
+            displayScrambledNumList();
+
+            keepPlaying = game.checkAnswer(getUserInput());
+            if(keepPlaying){
+                game.resetGameBoard(1);}
+        }
+        openActivitySubmitBtn();
+
     }
     public void openActivityPauseBtn() {
         Intent pausebtn = new Intent(this, MenuBtn.class);
         startActivity(pausebtn);
     }
+
     public void openActivitySubmitBtn() {
-
-        if (!false){
-            // game.checkanswer if false show end screen/popup, else reset gameboard and continue.
-
-            Intent submitBtn = new Intent(this, SubmitBtn.class);
-            startActivity(submitBtn);}
+        userStats.addScore(game.getScore());
+        Intent submitBtn = new Intent(this, SubmitBtn.class);
+        submitBtn.putExtra("userStats", userStats);
+        startActivity(submitBtn);
     }
+
 
     public void displayRandNumList() {
         for (int i = 0; i < game.getNumberList().size(); i++) {
@@ -67,13 +74,31 @@ public class MediumLevel extends AppCompatActivity {
     }
 
     public void displayScrambledNumList() {
-        for (int i = 0; i < game.getNumberList().size(); i++) {
-            pause(1500);
-            DisplayList.setText("" + game.getNumberList().get(i));
-            pause(1500);
-            DisplayList.setText("");
+        DisplayInstructions.setText("Lets test your memory!");
+        pause(1500);
+        ArrayList<Integer> scrambleList = game.scramble();
+        String numList = "";
+        for (int i = 0; i < scrambleList.size(); i++) {
+            numList += scrambleList.get(i);
         }
+        DisplayList.setText(numList);
     }
+
+    public ArrayList<Integer> getUserInput() {
+        ArrayList<Integer> answerUser = new ArrayList<Integer>();
+        for(int i = 0; i < game.getNumberList().size(); i++){
+            DisplayInstructions.setText("Enter Number " + (i+1) + " In the Sequence");
+            submitBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int num = Integer.parseInt(userInput.getText().toString());
+                    answerUser.add(num);
+                }
+            });
+        }
+        return answerUser;
+    }
+
 
     public void pause(int numMillisecond){
         try {
